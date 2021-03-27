d6b8f51cc0a0c8f77068afce5e924f8ce03dcde6
Mitch Sturba <57732799+MitchellSturba@users.noreply.github.com>
2021-03-27 17:18:47 -0400

    Create Login.py

---
+++ PythonTests/Login.py
@@ -0,0 +1,38 @@
+#Selenium webdriver tests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+#Set the path variables
+PATH = "./chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+#Get the link to the website we're testing
+driver.get("https://www.litcharts.com/")
+#print(driver.title)
+
+#Find the loginButton element
+link = driver.find_element_by_link_text("Sign In")
+link.click()
+
+#get the username input field
+user = driver.find_element_by_id("user[signin]")
+
+#Enter the string 'ticket' to search for results
+user.send_keys("SeleniumTest")
+
+#Now the password
+mypass = driver.find_element_by_id("user[password]")
+mypass.send_keys("SeleniumTests")
+
+#Hit enter to login
+mypass.send_keys(Keys.RETURN)
+
+time.sleep(5)
+
+#Exit the application
+driver.quit()
+
