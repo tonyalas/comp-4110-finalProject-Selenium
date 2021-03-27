67dface0d5b41c756e518fa36fd9e9fd80ed6745
Mitch Sturba <57732799+MitchellSturba@users.noreply.github.com>
2021-03-27 11:37:54 -0400

    Search.py fully works in printing books titles

--- PythonTests/search.py
+++ PythonTests/search.py
@@ -1,6 +1,9 @@
 #Selenium webdriver tests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 #Set the path variables
@@ -20,9 +23,23 @@ search.send_keys("George Orwell")
 #Hit enter to preform the actual search
 search.send_keys(Keys.RETURN)
 
-titles = driver.find_elements_by_class_name("title")
-for x in titles:
-	print(x.text)
+#gets all the hits
+hits = driver.find_elements_by_class_name("hit-item")
+
+#reads through hits
+for hit in hits:
+
+	#gets title in hits
+	title = hit.find_element_by_class_name("title")
+
+	#This whole block makes sure it's not printing the blurred text
+	tester = title.get_attribute('class')
+	check = False
+	if tester == 'title blurred-text':
+		continue
+	else:
+		print(title.text)
+
 
 time.sleep(5)
 
